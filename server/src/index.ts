ca6807ada5c3a47ab38074f94ec7b13fd917b7d4
Akshat <79667957+akshat0069@users.noreply.github.com>
2024-07-20 03:08:16 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,13 @@ import { productRouter } from "./routes/product";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 
 app.use("/user",userRouter);
 app.use("/product",productRouter);
